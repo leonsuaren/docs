19f2ffb16d34fb37f4a5dd1ab66287a53e18ac2f
Leon Suarez <leonsua@gmail.com>
2023-02-21 14:08:36 -0500

    installed styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <h1>Media</h1>
     </div>
   );
 }
