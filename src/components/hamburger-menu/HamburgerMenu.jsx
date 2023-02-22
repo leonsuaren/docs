10829d97184be4a361fb5fad2c45c25b6c83c8a4
Leon Suarez <leonsua@gmail.com>
2023-02-21 22:26:04 -0500

    finished the hamburger menu button

--- src/components/hamburger-menu/HamburgerMenu.jsx
+++ src/components/hamburger-menu/HamburgerMenu.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Line, HamburgerWrapper, HamburgerButton } from './styles.js';
+import React, { useState } from 'react';
+import { LineOne, LineTwo, LineThree, HamburgerWrapper, HamburgerButton } from './styles.js';
 
 export const HamburgerMenu = () => {
+  const [openMenu, setOpenMenu] = useState(true);
+
+  const handleOnOpenMenu = () => {
+    setOpenMenu(s => !s);
+  }
+
   return (
     <HamburgerWrapper>
-      <HamburgerButton>
-        <Line />
-        <Line />
-        <Line />
+      <HamburgerButton onClick={handleOnOpenMenu}>
+        <LineOne openMenu={openMenu}/>
+        <LineTwo openMenu={openMenu}/>
+        <LineThree openMenu={openMenu}/>
       </HamburgerButton>
     </HamburgerWrapper>
   )
--- src/components/hamburger-menu/styles.js
+++ src/components/hamburger-menu/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 
 export const HamburgerWrapper = styled.div`
   width: 100%;
@@ -13,13 +13,85 @@ export const HamburgerButton = styled.button`
   border: none;
   cursor: pointer;
   background-color: transparent;
+  position: relative;
 `;
 
-export const Line = styled.div`
-  width: 100%;
-  height: 5px;
+const rotateLineOne = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(90deg);
+  }
+`;
+
+const hamburgerAnimationOne = ({ openMenu }) => {
+  if (openMenu) {
+    return css`
+      transform: rotate(0deg) translate(0px, 0px);
+      `
+  } else {
+    return css`
+    transform: rotate(45deg) translate(6px, 5px);
+      `
+  }
+};
+
+const hamburgerAnimationTwo = ({ openMenu }) => {
+  if (openMenu) {
+    return css`
+      transform: translateX(0px);
+      opacity: 1;
+    `
+  } else {
+    return css`
+      transform: translateX(-20px);
+      opacity: 0;
+    `
+  }
+}
+
+const hamburgerAnimationThree = ({ openMenu }) => {
+  if (openMenu) {
+    return css`
+      transform: translateX(0px);
+    `
+  } else {
+    return css`
+      transform: rotate(-45deg) translate(7px, -7px);
+    `
+  }
+}
+
+export const LineOne = styled.div`
+  width: 40px;
+  height: 4px;
+  margin: 5px;
   border-radius: 5px;
   background-color: rgb(143, 142, 145);
-  margin: 5px;
-  width: 40px
+  transition: all .5s ease-in-out;
+  ${hamburgerAnimationOne}
 `;
+
+
+
+export const LineTwo = styled.div`
+  width: 40px;
+  height: 4px;
+  margin: 5px;
+  border-radius: 5px;
+  background-color: rgb(143, 142, 145);
+  transition: all .5s ease-in-out;
+  ${hamburgerAnimationTwo}
+`;
+
+export const LineThree = styled.div`
+  width: 40px;
+  height: 4px;
+  margin: 5px;
+  border-radius: 5px;
+  background-color: rgb(143, 142, 145);
+  transition: all .5s ease-in-out;
+  ${hamburgerAnimationThree}
+`;
+
